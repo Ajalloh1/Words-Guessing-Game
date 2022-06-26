65f337fb8ff9adeabb4715cc6eea720f9c2d9dc1
Ajalloh1 <mys4jalloh@gmail.com>
2022-06-26 12:03:01 -0400

    added some js

--- index.js
+++ index.js
@@ -128,3 +128,159 @@ function listChoices() {
 
     }
 }
+
+// Notify user that chosen answer is correct.//
+function correctAnswer() {
+    var correctNotify = document.createElement("div");
+    correctNotify.setAttribute("class", "border-top mt-3 pt-3")
+    correctNotify.setAttribute("style", "font-size: 12px; color: green; font-weight: bold;");
+    correctNotify.textContent = "You got the answer right!";
+    choicesContent.appendChild(correctNotify);
+}
+
+// alert user that choisen answer is wrong
+function incorrectAnswer() {
+    var incorrectNotify = document.createElement("div");
+    incorrectNotify.setAttribute("class", "border-top mt-3 pt-3");incorrectNotify.setAttribute("style", "font-size: 12px; color: red; font-weight: bold;");
+    incorrectNotify.textContent = "You got the answer wrong!";
+    choicesContent.appendChild(incorrectNotify);
+}
+
+// The timer that counts down when the game is started
+function countdownClock() {
+    var timerInterval = setInterval(function() {
+        // Display time and decrease by second
+        gameClock.textContent = gameTimer;
+        gameTimer--;
+
+        // Once the timer hits zero, game is ended
+        if (gameTimer <= 0) {
+            clearInterval(timerInterval);
+            gameClock.textContent = "0";
+            choicesContent.innerHTML = " ";
+            questionNumber = 0;
+            choicesContent.setAttribute("display", "none");
+            startMenu.setAttribute("style", "display: block;");
+            questionHeading.textContent = "Your score is: " +  gameTimer;
+            gameTimer = numberOfQuestions * 15;
+        } 
+        // Freeze clock if user runs through all the questions and end game
+        else if (questionNumber === 10) {
+            clearInterval(timerInterval);
+            // Reset stats so user can start a new game
+            questionNumber = 0;
+            gameTimer = numberOfQuestions * 15;
+        }
+
+    }, 1000);
+} 
+
+
+// Add event to the button choices and see if what the 
+// user clicks matches the answer in the questions array
+document.addEventListener("click", function(event) {
+    if (event.target.matches('.choice-btn')) {
+        // console.log(event.target.textContent);
+        event.stopPropagation();
+        event.preventDefault();
+        // Condition if user selects correct answer
+        if (event.target.textContent === questions[questionNumber].answer) {
+            
+            
+
+            // Move on to the next question
+            questionNumber = questionNumber + 1;
+            // Add time to the clock
+            gameTimer += 5;
+
+            if (questionNumber <= (numberOfQuestions - 1)) {
+                questionHeading.textContent = questions[questionNumber].title;
+                    // Run function to clear buttons
+                    // and list new choices
+                choicesContent.innerHTML = " ";
+                listChoices();
+                // Inform user that they got the right answer
+                correctAnswer();
+                // console.log("Question Number: " + questionNumber);
+            } else {
+                // End of game so clear any trace of choices
+                choicesContent.innerHTML = " ";
+                // Inform user that they got the right answer
+                correctAnswer();
+                // Bring up input for user to enter in their high score
+                enterInitialsMenu.setAttribute("style", "display: block;");
+                // Allow user to restart quiz
+                startMenu.setAttribute("style", "display: block;");
+                viewHighScoresLink.setAttribute("style", "display: inline;");
+                // Display the user's final score
+                questionHeading.textContent = "Your score is: " +  gameTimer;
+                // User's final score is equal to the time left in the game
+                finalScore = gameTimer;
+            }
+
+            
+        } 
+        // Condition if user selects wrong answer
+        else if (event.target.textContent !== questions[questionNumber].answer) {
+            
+            // Move on to the next question
+            questionNumber = questionNumber + 1;
+            // Remove time from the clock
+            gameTimer -= 15;
+
+            if (questionNumber <= (numberOfQuestions - 1)) {
+                questionHeading.textContent = questions[questionNumber].title;
+                    // Run function to clear buttons
+                    // and list new choices
+                choicesContent.innerHTML = " ";
+                listChoices();
+                // Inform user that they got the wrong answer
+                incorrectAnswer();
+            } else {
+                // End of game so clear any trace of choices
+                choicesContent.innerHTML = " ";
+                // Inform user that they got the wrong answer
+                incorrectAnswer();
+                // Bring up input for user to enter in their high score
+                enterInitialsMenu.setAttribute("style", "display: block;");
+                // Allow user to restart quiz
+                startMenu.setAttribute("style", "display: block;");
+                viewHighScoresLink.setAttribute("style", "display: inline;");
+                // Display the user's final score
+                questionHeading.textContent = "Your score is: " +  gameTimer;
+                // User's final score is equal to the time left in the game
+                finalScore = gameTimer;
+            }
+            
+            
+        }
+    }
+});
+
+function enterInitials(event) {
+    event.preventDefault();
+    // Take the value the user enters into the input after game ends
+    var userInitials = document.getElementById('initials-input').value;
+    
+    // Object containing the user initials and final score
+    var userScores = {
+        initials: userInitials,
+        score: finalScore
+    };
+
+    // Push the above object into the high scores array
+    highScores.push(userScores);
+    // console.log(highScores);
+
+    // Convert the object into a string
+    var highScoresString = JSON.stringify(highScores);
+
+    // Store the string into the user's local storage
+    window.localStorage.setItem("high scores", highScoresString);
+
+    // Inform user their score is now entered
+    questionHeading.textContent = "You have entered your score. Play again?";
+    enterInitialsMenu.setAttribute("style", "display: none;");
+    choicesContent.innerHTML = " ";
+
+}
