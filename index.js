a115cdbbcfb1c1048055ba40cc6ebed526f056f8
Ajalloh1 <mys4jalloh@gmail.com>
2022-06-26 09:08:37 -0400

    chaged selectors and ids to acommate all sections

---
+++ index.js
@@ -0,0 +1,10 @@
+// refrence elements on html page 
+var choicesContent = document.querySelector("#choices-menu");
+var startMenu = document.getElementById('start-menu');
+var questionHeading = document.getElementById('heading1');
+var gameClock = document.getElementById('game-clock');
+var enterInitialsMenu = document.getElementById('enter-initials-menu');
+var enterInitialsBtn = document.getElementById('submit-intials-btn');
+var scoresMenu = document.getElementById('scores-menu');
+var backToStartLink = document.getElementById('back-to-start-link');
+var viewHighScoresLink = document.getElementById('high-scores-link');
